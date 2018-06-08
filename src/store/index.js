5ebfe11abf4e436a63d5aa186fdc907e32474ff2
chester <chester.ko@kakaocorp.com>
2018-06-08 15:32:24 +0900

    쓸모없는 주석 삭제

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,3 @@
-/*
-
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import storeFactory from './store'
-
-const store = storeFactory()
-
-window.React = React
-window.store = store
-
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-)
-*/
 
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { reducer_query, reducer_view } from './reducers'
@@ -33,23 +13,6 @@ const logger = store => next => action => {
     console.groupEnd()
     return result
 }
-
-/*
-const saver = store => next => action => {
-    let result = next(action)
-    localStorage['redux-store'] = JSON.stringify(store.getState())
-    return result
-}
-
-const storeFactory = (initialState=stateData) =>
-    applyMiddleware(logger, saver)(createStore)(
-        combineReducers({colors, sort}),
-        (localStorage['redux-store']) ?
-            JSON.parse(localStorage['redux-store']) :
-            stateData
-    )
-
-*/
 
 const storeFactory = (initialState={}) =>
     applyMiddleware(logger)(createStore)(
